8f996517b51271840ab5eb20870c772d7278c91b
Joseph Elibri <elibrij@gmail.com>
2023-12-09 00:45:33 -0500

    view displays combined frequency graph and more information refactored view/window.py

--- view/window.py
+++ view/window.py
@@ -1,13 +1,14 @@
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
+import pathlib
+
 import pyglet
 from pyglet.gui.widgets import PushButton
 from pyglet.image import ImageData
-from pyglet.shapes import BorderedRectangle
 from pyglet.text import Label
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
-import pathlib
-from tkinter.filedialog import askopenfilename
-from tkinter.messagebox import showerror
+
 import controller
 from .widgets import SliderButton
 
@@ -16,21 +17,23 @@ assets = pathlib.Path(__file__).parents[1] / "assets"
 pyglet.resource.path = [str(assets)]
 pyglet.resource.reindex()
 
+# loads all required assets
 load_file_image = pyglet.resource.image("load_file_image.png")
 left_slider_button = pyglet.resource.image("blue_slider.png")
 left_slider_hover = pyglet.resource.image("blue_slider_hover.png")
-
 right_slider_button = pyglet.resource.image("blue_slider.png", flip_x=True)
 right_slider_button.anchor_x = 0
 right_slider_hover = pyglet.resource.image("blue_slider_hover.png", flip_x=True)
 right_slider_hover.anchor_x = 0
-
 tall_rectangle = pyglet.resource.image("small_rectangle.png")
 wide_rectangle = pyglet.resource.image("big_rectangle.png")
-
 icon = pyglet.resource.image("speaker_icon.png")
 
 def figure_to_image(fig: Figure) -> ImageData:
+    """
+    converts a matplotlib Figure into an image that can be drawn to the 
+    window with pyglet
+    """
     canvas = FigureCanvasAgg(fig)
     data, (width, height) = canvas.print_to_buffer()
     image = ImageData(width, height, "RGBA", data, -4*width)
@@ -43,6 +46,9 @@ class AppWindow(pyglet.window.Window):
     Contains all relevant ui elements and functions for the view
     """
     def __init__(self):
+        """
+        Creates a new window instance that will be opened when pyglet.app.run is called
+        """
         super().__init__(1200, 675, caption="Audio Analyzer")
         self.set_icon(icon)
 
@@ -57,43 +63,33 @@ class AppWindow(pyglet.window.Window):
                                   "Low Frequency",
                                   "Mid Frequency",
                                   "High Frequency",
-                                  "Power Spectrum",
+                                  "All Frequencies",
                                   ]
         self.rt_60s: list[float] = []
-
-        self.gui_batch = pyglet.graphics.Batch()
+        
         # frame for gui elements that are always present and never change
+        self.gui_batch = pyglet.graphics.Batch()
         self.static_gui_frame = pyglet.gui.Frame(self, cell_size=100)
 
+        # create the "Load File" button and give it a function to call when pressed
         self.load_file_button = PushButton(0, 575, pressed=load_file_image, depressed=load_file_image, batch=self.gui_batch)
         self.load_file_button.set_handler('on_press', self._on_load_file_press)
+        self.static_gui_frame.add_widget(self.load_file_button)
 
+        # put rectangles where they need to go
         self.wide_rect = pyglet.sprite.Sprite(wide_rectangle, x=0, y=0, batch=self.gui_batch)
         self.tall_rect = pyglet.sprite.Sprite(tall_rectangle, x=300, y=575, batch=self.gui_batch)
 
+        # creates text labels
         self.label_batch = pyglet.graphics.Batch()
-        self.file_label = Label("File: ", "Calibri", font_size=25, color=(0, 0, 0, 255), x=15, y=12.5, width=100, height=20, batch=self.label_batch, dpi=100)
-        self.time_label = Label("Time: ", "Calibri", font_size=25, color=(0, 0, 0, 255), x=600, y=12.5, width=100, height=20, batch=self.label_batch, dpi=100)
-        self.rt60_label = Label("RT60: ", "Calibri", font_size=25, color=(0, 0, 0, 255), x=900, y=12.5, width=75, height=20, batch=self.label_batch, dpi=100)
+        self.file_label = Label("File: ", "Calibri", font_size=15, color=(0, 0, 0, 255), x=15, y=12, width=100, height=15, batch=self.label_batch, dpi=100)
+        self.time_label = Label("Time: ", "Calibri", font_size=15, color=(0, 0, 0, 255), x=15, y=30, width=100, height=15, batch=self.label_batch, dpi=100)
+        self.rt60_label = Label("RT60: ", "Calibri", font_size=15, color=(0, 0, 0, 255), x=400, y=12, width=100, height=15, batch=self.label_batch, dpi=100)
+        self.difference_label = Label("RT60 Difference: ", font_size=15, color=(0, 0, 0, 255), x=400, y=30, width=100, height=15, batch=self.label_batch, dpi=100)
+        self.frequency_label = Label("Highest Resonant Frequency: ", font_size=15, color=(0, 0 , 0, 255), x=700, y=30, width=100, height=15, batch=self.label_batch, dpi=100)
         self.title_label = Label("", "Calibri", font_size=50, color=(0, 0, 0, 255), x=300, y=600, width=900, height=100, align='center', batch=self.label_batch, dpi=100)
 
-        self.static_gui_frame.add_widget(self.load_file_button)
-
-    def _on_load_file_press(self):
-        """
-        Called when ever the load file button is pressed
-
-        Will pass path of selected file to the controller, otherwise does nothing
-        """
-        if not self.choosing_file:
-            self.choosing_file = True
-            chosen_file: str = askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3")], title="Select File")
-            self.choosing_file = False
-            if chosen_file != "":
-                chosen_path = pathlib.Path(chosen_file)
-                self.current_file = chosen_path.name
-                controller.loadFile(chosen_file)
-
+    
     def on_draw(self):
         """
         Overwrites on_draw method in pyglet.window.Window
@@ -106,11 +102,12 @@ class AppWindow(pyglet.window.Window):
         self.label_batch.draw()
 
     def update_images(self, data: tuple[list[Figure], list[float], float] | None):
-        # TODO improve update_images documentation
         """
         Called from model
 
         receives relevant information from the model and processes accordingly
+
+        expects a tuple containing a list of the figures, a list of the rt60 values, the length of the audio in seconds, and highest res frequency
         """
         if data is None:
             showerror("Error", "Unexpected error when opening file")
@@ -129,10 +126,27 @@ class AppWindow(pyglet.window.Window):
         self._update_rt60_label()
         self._update_title_label()
         self._update_file_label(self.current_file)
+        self._update_difference_label((sum(self.rt_60s)/3) - 0.5)
+        self._update_frequency_label(data[3])
 
         if not self.image_loaded:
             self._create_sliders()
             self.image_loaded = True
+
+    def _on_load_file_press(self):
+        """
+        Called when ever the load file button is pressed
+
+        Will pass path of selected file to the controller, otherwise does nothing
+        """
+        if not self.choosing_file:
+            self.choosing_file = True
+            chosen_file: str = askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3")], title="Select File")
+            self.choosing_file = False
+            if chosen_file != "":
+                chosen_path = pathlib.Path(chosen_file)
+                self.current_file = chosen_path.name
+                controller.loadFile(chosen_file)
             
 
     def _create_sliders(self):
@@ -180,17 +194,41 @@ class AppWindow(pyglet.window.Window):
         self._update_title_label()
 
     def _update_time_label(self, time):
+        """
+        Update displayed length of file in seconds
+        """
         self.time_label.text = f"Time: {time:0.2f}s"
     
     def _update_rt60_label(self):
+        """
+        Update displayed rt60 value
+        """
         rt_index = self.image_index - 2
         if rt_index >= 0 and rt_index <= 2:
             self.rt60_label.text = f"RT60: {self.rt_60s[rt_index]:0.2e}s"
         else:
             self.rt60_label.text = "RT60: NA"
 
+    def _update_difference_label(self, value):
+        """
+        Update rt_60 difference label
+        """
+        self.difference_label.text = f"RT60 Difference: {value:0.2e}s"
+
+    def _update_frequency_label(self, value):
+        """
+        Update displayed highest resonant frequency value
+        """
+        self.frequency_label.text = f"Highest Resonant Frequency: {int(value)}Hz"
+
     def _update_title_label(self):
+        """
+        Update figure title label
+        """
         self.title_label.text = self.titles[self.image_index]
 
     def _update_file_label(self, filename):
-        self.file_label.text = f"File: {filename:.20}"
+        """
+        Update displayed filename
+        """
+        self.file_label.text = f"File: {filename:.30}"
